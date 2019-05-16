79f6fc1923171a331b623bd12982724791ea229e
Diego Bruning <diego.brg92@gmail.com>
2019-05-16 13:04:15 -0300

    initiating IPv6 class implementation

--- general.py
+++ general.py
@@ -3,6 +3,7 @@ import textwrap
 
 # Returns MAC as string from bytes (ie AA:BB:CC:DD:EE:FF)
 def get_mac_addr(mac_raw):
+    # need python 3
     byte_str = map('{:02x}'.format, mac_raw)
     mac_addr = ':'.join(byte_str).upper()
     return mac_addr
